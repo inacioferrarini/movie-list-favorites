24a778eae9e25fb396eab01ac8afa9b5506d18b7
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-07 17:24:41 -0300

    Updates coordinator

--- Favorites/Coordinators/FavoritesCoordinator.swift
+++ Favorites/Coordinators/FavoritesCoordinator.swift
@@ -8,7 +8,26 @@ public class FavoritesCoordinator: Coordinator {
         self.tabBar = tabBar
     }
     
+    lazy var tabBarItem: UITabBarItem? = {
+        return UITabBarItem(title: "Favorites",
+                            image: Assets.Icons.Modules.Favorite,
+                            selectedImage: nil)
+    }()
+    
+    public lazy var viewController: UIViewController = {
+//        guard let vc = MovieCatalogViewController.instantiate() else { return UIViewController() }
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor.red
+        if let tabBarItem = self.tabBarItem {
+            vc.tabBarItem = tabBarItem
+        }
+        return vc
+    }()
+
     public func start() {
+        var viewControllers = tabBar.viewControllers ?? []
+        viewControllers += [self.viewController]
+        tabBar.viewControllers = viewControllers
         print("starting FavoritesCoordinator ... ")
     }
     
