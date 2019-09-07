58809404dfbaff04091733831a6b65275118abd8
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-07 11:05:50 -0300

    Initial Commit

---
+++ Favorites/Favorites.h
@@ -0,0 +1,19 @@
+//
+//  Favorites.h
+//  Favorites
+//
+//  Created by José Inácio Athayde Ferrarini on 07/09/19.
+//  Copyright © 2019 José Inácio Athayde Ferrarini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Favorites.
+FOUNDATION_EXPORT double FavoritesVersionNumber;
+
+//! Project version string for Favorites.
+FOUNDATION_EXPORT const unsigned char FavoritesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Favorites/PublicHeader.h>
+
+
