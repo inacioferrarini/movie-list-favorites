8c0d917d6d90b13785741d7ec4424810119e26c5
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-28 19:33:28 -0300

    Updates iOS target to 12.0; Updates Swift version to 4.2

--- FavoriteSymbols.swift
+++ FavoriteSymbols.swift
@@ -1,6 +1,6 @@
 //    The MIT License (MIT)
 //
-//    Copyright (c) 2017 Inácio Ferrarini
+//    Copyright (c) 2019 Inácio Ferrarini
 //
 //    Permission is hereby granted, free of charge, to any person obtaining a copy
 //    of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,12 @@
 
 import Foundation
 
+///
 /// Key for FavoriteMovies Ids
+///
 public let FavoriteMoviesIdsKey = "favoriteMovies"
+
+///
 /// Type for FavoriteMovies Ids
+///
 public typealias FavoriteMoviesIdsTypes = [Int]
