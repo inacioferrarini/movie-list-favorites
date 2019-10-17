87ea2c128f5d52fc346d94bdc43d9f5b5b522119
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-10-16 23:02:05 -0300

    Migrated to XibView

--- Favorites/View/FilterOptionsView.swift
+++ Favorites/View/FilterOptionsView.swift
@@ -31,11 +31,10 @@ protocol FilterOptionsViewDelegate: AnyObject {
 
 }
 
-class FilterOptionsView: UIView {
+class FilterOptionsView: XibView {
 
     // MARK: - Outlets
 
-    @IBOutlet weak private(set) var contentView: UIView!
     @IBOutlet weak private(set) var tableView: UITableView!
 
     // MARK: - Private Properties
@@ -60,39 +59,7 @@ class FilterOptionsView: UIView {
 
     // MARK: - Initialization
 
-    ///
-    /// Initializes the view with using `UIScreen.main.bounds` as frame.
-    ///
-    public required init() {
-        super.init(frame: UIScreen.main.bounds)
-        commonInit()
-    }
-
-    ///
-    /// Initializes the view with using the given `frame`.
-    /// - Parameter frame: Initial view dimensions.
-    ///
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
-    }
-
-    ///
-    /// Initializes the view with using the given `coder`.
-    /// - Parameter aDecoder: NSCoder to be used.
-    ///
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        commonInit()
-    }
-
-    private func commonInit() {
-        let bundle = Bundle(for: type(of: self))
-        let className = String(describing: type(of: self))
-        bundle.loadNibNamed(className, owner: self, options: nil)
-        addSubview(contentView)
-        contentView.frame = self.bounds
-        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    override open func setupView() {
         setup()
         setupTableView()
     }
